bc9ef53b5c0fccc4cbb0c5b0feec518aa82faf41
daRoyalCacti <srdrmrdave@gmail.com>
2022-05-28 09:37:55 +1000

    Added methods to interpret the cnns

--- cnn.py
+++ cnn.py
@@ -62,8 +62,24 @@ class CNN(nn.Module):
         output_w4 = get_output_size(output_w3, pooling_size, 0, 1, pooling_size)
         output_h4 = get_output_size(output_h3, pooling_size, 0, 1, pooling_size)
 
-        self.out = nn.Linear(int(channels2 * output_w4 * output_h4),
-                             int(no_classes))  # fully connected layer, output 10 classes
+        self.last_layer_size = int(channels2 * output_w4 * output_h4)
+
+        self.out = nn.Linear(self.last_layer_size, int(no_classes))  # fully connected layer, output 10 classes
+
+    def get_last_layer(self, x):
+        if x.get_device() >= 0:
+            output = torch.zeros(x.shape[0], self.last_layer_size, device=x.get_device())
+        else:
+            output = torch.zeros(x.shape[0], self.last_layer_size)
+        for k in range(output.shape[0]):
+            xp = torch.reshape(x[k], (1, x[k].shape[0], x[k].shape[1], x[k].shape[2])).float()
+            xp = self.conv1(xp)
+            # x = self.conv1(x)
+            xp = self.conv2(xp)
+            xp = xp.view(xp.size(0), -1)  # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+            # output = self.out(x)
+            output[k, :] = xp
+        return output
 
     def forward(self, x):
         if x.get_device() >= 0:
@@ -173,7 +189,81 @@ def learn_cnn_standard(X_tr, y_tr, X_tst, y_tst, extra_data, output_file):
             counter += 1
 
     log_scores(res.detach().numpy().astype(int), y_tst, extra_data, output_file)
+    interpret_cnn(cnn, X_tr, device)
+
+
+global_counter = 0
+
+
+def interpret_cnn(cnn, X_tr, device):
+    X_tr = torch.reshape(torch.from_numpy(X_tr), (X_tr.shape[0], X_tr.shape[3], X_tr.shape[2], X_tr.shape[1]))
+    res = torch.zeros(X_tr.shape[0], cnn.last_layer_size)
+    data_loader = DataLoader(DatasetWrapper1D(X_tr), batch_size=200, shuffle=True)
+    counter = 0
+    for x_batch in data_loader:
+        gpu_x = x_batch.float().to(device)
+        # getting the results from the CNN when run on the training data
+        # need to take the most likely one
+        output = cnn.get_last_layer(gpu_x).detach().cpu()
+        for i in range(output.shape[0]):
+            res[counter] = output[i]  # net(x_train)
+            counter += 1
+    res = res.detach().numpy()
+    print_weights(cnn)
+    get_nearest_neighbours(res, [i for i in range(20)], 50)
+
+
+def print_weights(cnn):
+    global global_counter
+    file = open('../results/cnn_anal/standard/' + str(global_counter) + '_weights.txt', 'w')
+
+    w1 = cnn.conv1[0].weight.data
+    w2 = cnn.conv2[0].weight.data
+
+    file.write("w1s\n===============================================\n")
+    for i in range(w1.shape[0]):
+        for j in range(w1.shape[1]):
+            file.write("Matrix : (" + str(i) + "," + str(j) + ")\n")
+            for k in range(w1.shape[2]):
+                for h in range(w1.shape[3]):
+                    file.write(str(w1[i, j, k, h].item()) + ", ")
+                file.write("\n")
+    file.write("w2s\n===============================================\n")
+    for i in range(w2.shape[0]):
+        for j in range(w2.shape[1]):
+            file.write("Matrix : (" + str(i) + "," + str(j) + ")\n")
+            for k in range(w2.shape[2]):
+                for h in range(w2.shape[3]):
+                    file.write(str(w2[i, j, k, h].item()) + ", ")
+                file.write("\n")
+
+    file.close()
+
+
+def get_nearest_neighbours(res, inds, k):
+    global global_counter
+    errors = np.zeros((len(inds), res.shape[0]))
+    for i in range(len(inds)):
+        img = res[inds[i]]
+        for j in range(res.shape[0]):
+            if i == j:
+                errors[i, j] = 1000000000
+            else:
+                errors[i, j] = np.sum((img - res[j]) ** 2)
+
+    file = open('../results/cnn_anal/standard/' + str(global_counter) + '.txt', 'w')
+    for i in range(len(inds)):
+        file.write("Nearest neighbours for image : " + str(inds[i]) + "\n")
+        closest_inds = np.argsort(errors[i])
+        for j in range(k):
+            file.write(str(j) + " closest : " + str(closest_inds[j]) + "\n")
+        file.write("\n")
+    file.close()
+    global_counter += 1
 
 
 def run_cnn_standard():
+    global global_counter
+    global_counter = 0
     compute_output_all_image(learn_cnn_standard, "../results/cnn.txt")
+    global_counter = 0
